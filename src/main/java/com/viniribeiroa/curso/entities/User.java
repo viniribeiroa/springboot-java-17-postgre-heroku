56f7ac0bfac4631069856e8baa8863f9c70020f7
viniribeiroa <viniribeiroa@gmail.com>
2022-07-28 22:54:47 -0300

    correção conexão com db

--- src/main/java/com/viniribeiroa/curso/entities/User.java
+++ src/main/java/com/viniribeiroa/curso/entities/User.java
@@ -7,9 +7,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 
 @Entity
+@Table(name = "users")
 public class User implements Serializable{
 	
 	
@@ -22,6 +24,7 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
+	
 	
 	public User() {
 		

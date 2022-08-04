1566b3f0d905ee4171b13e29709ac395c59f7c92
viniribeiroa <viniribeiroa@gmail.com>
2022-08-03 23:15:59 -0300

    Product

---
+++ src/main/java/com/viniribeiroa/curso/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.viniribeiroa.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.viniribeiroa.curso.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
---
+++ src/main/java/com/viniribeiroa/curso/services/ProductService.java
@@ -0,0 +1,29 @@
+package com.viniribeiroa.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.viniribeiroa.curso.entities.Product;
+import com.viniribeiroa.curso.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+		
+	}
+	
+	public Product findById(Long id) {
+		
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}

547009a00de52d22b3621fee3e98530a80d461aa
viniribeiroa <viniribeiroa@gmail.com>
2022-08-04 23:11:35 -0300

    Payment, one-to-one association

--- src/main/java/com/viniribeiroa/curso/config/TestConfig.java
+++ src/main/java/com/viniribeiroa/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.viniribeiroa.curso.entities.Category;
 import com.viniribeiroa.curso.entities.Order;
 import com.viniribeiroa.curso.entities.OrderItem;
+import com.viniribeiroa.curso.entities.Payment;
 import com.viniribeiroa.curso.entities.Product;
 import com.viniribeiroa.curso.entities.User;
 import com.viniribeiroa.curso.entities.enums.OrderStatus;
@@ -83,6 +84,12 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-07-21T03:42:10Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	} 
 
 }
